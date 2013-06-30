9515db659771e20030dd22cbb368fc89e88f8f7c
Eric Artzt <artzte@baddabigboom.com>
2013-06-30 08:14:47 -0700

    Initial skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HandlebarsAssetsApp::Application.config.secret_token = '6bb139084f1776a711c3342027eb3f2bdbbe3130fc0f04ea9a1f82562225307c0d9462a76cc7e84ed5377599797bfc0357e5b09de57b21c1e1e9833e358efd88'
